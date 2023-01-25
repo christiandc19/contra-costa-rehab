1f7a1b79715d0410a213a275b2c8222e43d74a84
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-01-24 20:29:38 -0800

    styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,7 +14,7 @@ const About = () => {
                             <Fade top>
                                 <h1>WHO WE ARE</h1>
 
-                                <p>Contra Costa Rehab Center lets you taper in a safe, structured manner. We use evidence-based medical practices for controlling withdrawal symptoms and watch over you on a 24-7 basis. Our nursing team is adept at administering medications for gradual tapering. And whenever you feel up to it, you also have the chance to participate in therapy and peer support groups to explore the underlying triggers of addiction and associated mental health disorders. </p>
+                                <p>Contra Costa Rehab Center strives to make sobriety less stressful by focusing on sobriety. We utilize established medical treatments to help you get sober and stay sober. </p>
                             </Fade>
                         </div>
                     </div>
@@ -27,7 +27,7 @@ const About = () => {
                                     <Fade top>
                                     <p className="container">
                                     <br/>
-                                    In many cases, medically supervised detox near Los Angeles is the right starting point for overcoming addiction to alcohol or dependence of prescription drugs or narcotics. Contra Costa Rehab Treatment Center strives to make this step less stressful by focusing on your comfort and safety first. Our compassionate staff utilizes established medical treatments to decrease withdrawal effects in an empathetic environment. In the meantime, you’ll receive behavioral health care from experienced counselors to boost your chances for an effective recovery. Detoxing by yourself or going abruptly is often dangerous. 
+                                    Contra Costa Rehab Center helps you taper in a safe, structured environment. We are there for you, through every stage of recovery. No matter where you’re located, you can reach out and get in touch with someone from our staff today.
                                     </p><br/>
                                     </Fade>
                                 </div>
